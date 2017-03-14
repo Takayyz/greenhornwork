12f75939994b375f0bfb2698fc051fc6d2d98a5a
hironeko <hiroshikitahara@HiroshiKitahara-no-MacBook-Pro.local>
2017-03-14 17:18:33 +0900

    create migrations

--- database/migrations/2017_03_14_070728_create_users_info.php
+++ database/migrations/2017_03_14_070728_create_users_info.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsersInfo extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_info', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('first');
+            $table->string('last');
+            $table->string('email')->unique();
+            $table->integer('tel');
+            $table->integer('store_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users_info');
     }
 }
