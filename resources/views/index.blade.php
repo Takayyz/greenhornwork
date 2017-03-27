cf535744748332b94fe2185fd6b4b0c123a5ab07
Komatsu Nobuyuki <abr0811@icloud.com>
2017-03-27 16:08:38 +0900

    ユーザーログイン後のビュー作成、ユーザー及び管理画面ヘッダーのアンカーを修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,15 @@
 
 @section('content')
 
+<div class="container">
+  <div class="row container__inner">
+      <div class="col-md-3 col-md-offset-2 col-xs-8 col-xs-offset-2">
+        <a href="{{ route('schedule.index') }}" class="btn btn-work__schedules user__btn"><i class="fa fa-briefcase" aria-hidden="true"></i>&nbsp;&nbsp;勤退</a>
+      </div>
+      <div class=" col-md-3 col-md-offset-2 col-xs-8 col-xs-offset-2">
+        <a href="{{ route('report.index') }}" class="btn btn__daily__reports user__btn"><i class="fa fa-file-text-o" aria-hidden="true"></i>&nbsp;&nbsp;日報</a>
+      </div>
+  </div>
+</div>
+
 @endsection
