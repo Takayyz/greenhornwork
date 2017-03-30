86443acf9b1823fc7e7b36609ea35282cdf575cf
Atsushi <atsushi88885@gmail.com>
2017-03-30 18:16:22 +0900

    emptyをSession::hasに修正

--- resources/views/work_schedule/edit.blade.php
+++ resources/views/work_schedule/edit.blade.php
@@ -8,12 +8,12 @@
   <p class="pull-right"><a href="{{ route('schedule.index') }}">一覧に戻る</a></p>
   <div class="panel-body">
     {!! Form::open(['route' => ['schedule.update', $schedule->id], 'method' => 'put' , 'files' => 'true']) !!}
-      <div class="form-group {{ !empty($errors->first('year')) ? 'has-error' :'' }}">
+      <div class="form-group {{ Session::has($errors->first('year')) ? 'has-error' :'' }}">
         {!! Form::label('year', '年'); !!}
         {!! Form::selectRange('year', date('Y'), date('Y')+20, $schedule->year ) !!}
         <span class="help-block">{{ $errors->first('year') }}</span>
       </div>
-      <div class="form-group {{ !empty($errors->first('month')) ? 'has-error' :'' }}">
+      <div class="form-group {{ Session::has($errors->first('month')) ? 'has-error' :'' }}">
         {!! Form::label('month', '月'); !!}
         {!! Form::selectRange('month', 1, 12, $schedule->month ) !!}
         <span class="help-block">{{ $errors->first('month') }}</span>
@@ -26,7 +26,7 @@
       <div class="form-group">
         <span class="help-block">※勤務表の変更が不要な場合はファイルを選択する必要はありません</span>
       </div>
-      <div class="form-group {{ !empty($errors->first('schedule')) ? 'has-error' :'' }}">
+      <div class="form-group {{ Session::has($errors->first('schedule')) ? 'has-error' :'' }}">
         {!! Form::file('schedule', null) !!}
         <span class="help-block">{{ $errors->first('schedule') }}</span>
       </div>
