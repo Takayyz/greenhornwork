82eed3d81ac33defd00a14b0590bad13c6b907c3
Atsushi <atsushi88885@gmail.com>
2017-03-30 13:43:56 +0900

    hrefのルート修正 / fileのformにrequiredを追加

--- resources/views/work_schedule/create.blade.php
+++ resources/views/work_schedule/create.blade.php
@@ -1,27 +1,29 @@
 @extends('partials.user_nav')
 
 @section('content')
+
 <div class="container">
   <div class="panel-heading">
     <h2>勤務表アップロード</h2>
   </div>
-  <p class="pull-right"><a href="{{ url('schedule') }}">一覧に戻る</a></p>
+  <p class="pull-right"><a href="{{ route('schedule.index') }}">一覧に戻る</a></p>
   <div class="panel-body">
     {!! Form::open(['url' => 'schedule', 'method' => 'post', 'files' => 'true']) !!}
       <div class="form-group {{ !empty($errors->first('year')) ? 'has-error' :'' }}">
         {!! Form::label('year', '年'); !!}
         {!! Form::selectRange('year', date('Y'), date('Y')+20, old('year', date('Y'))) !!}
         <span class="help-block">{{ $errors->first('year') }}</span>
-
       </div>
-        <div class="form-group {{ !empty($errors->first('month')) ? 'has-error' :'' }}">
-          {!! Form::label('month', '月'); !!}
-          {!! Form::selectRange('month', 1, 12, date('m')) !!}
-          <span class="help-block">{{ $errors->first('month') }}</span>
-        </div>
-
+      <div class="form-group {{ !empty($errors->first('month')) ? 'has-error' :'' }}">
+        {!! Form::label('month', '月'); !!}
+        {!! Form::selectRange('month', 1, 12, date('m')) !!}
+        <span class="help-block">{{ $errors->first('month') }}</span>
+      </div>
+      <div class="has-error">
+        <span class="help-block">@if(Session::has('message')) {{ Session('message') }} @endif</span>
+      </div>
       <div class="form-group {{ !empty($errors->first('schedule')) ? 'has-error' :'' }}">
-        {!! Form::file('schedule', null, ['required', 'class' => 'form-control']) !!}
+        {!! Form::file('schedule', ['required' => 'required']) !!}
         <span class="help-block">{{ $errors->first('schedule') }}</span>
       </div>
       <button type="submit" class="btn btn-success pull-right">追加</button>
