7eea2682aec77aeaa9f45106fc1e1c87e4babee9
ryoryou <atsut001@cougars.csusm.edu>
2017-04-05 00:15:59 -0700

    Update: getReportByDateRange has been modified (e.g. parameters position has changed)

--- app/Repositories/DailyReportsRepositoryEloquent.php
+++ app/Repositories/DailyReportsRepositoryEloquent.php
@@ -49,12 +49,22 @@ class DailyReportsRepositoryEloquent extends BaseRepository implements DailyRepo
       return $reports;
     }
 
-    public function getReportByDateRange($userId, $start_date, $end_date) {
-      $searchedReport = $this->model
-                                ->find($userId)
-                                ->dateRange($start_date, $end_date)
-                                ->get();
-
+    public function getReportByDateRange($start_date, $end_date, $userId = null) {
+      if(empty($userId)){
+        $searchedReport = $this->model
+                                  ->dateRange($start_date, $end_date)
+                                  ->get();
+      } else {
+        //　値が両方空ならそのユーザーに関するレポートデータ全てを取得
+        if(empty($start_date) && empty($end_date)) {
+          $searchedReport = $this->model->find($userId)->get();
+        } else {
+          $searchedReport = $this->model
+                                    ->find($userId)
+                                    ->dateRange($start_date, $end_date)
+                                    ->get();
+        }
+      }
       return $searchedReport;
     }
 }
