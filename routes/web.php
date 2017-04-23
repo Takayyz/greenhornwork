a9aadf980d4a8c74c712b2a87fed0aecc7e2f5f6
Atsushi <atsushi88885@gmail.com>
2017-04-24 00:59:22 +0900

    管理者のパスワードリセットルート追加

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::group(['prefix' => '/'], function() {
 });
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.' ,'namespace' => 'Admin'], function() {
-  Route::get('login', 'Auth\LoginController@showLoginForm');
-  Route::post('login', 'Auth\LoginController@login');
-  Route::post('logout', 'Auth\LoginController@logout');
+  Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+  Route::post('login', 'Auth\LoginController@login')->name('login');
+  Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
   Route::get('/', 'HomeController@index');
   Route::resource('report', DailyReportController::class);
@@ -37,6 +37,10 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.' ,'namespace' => 'Admin'], fu
   Route::get('adminuser/{adminuser}/mailedit', 'AdminUserController@mailedit')->name('adminuser.mailedit');
   Route::post('adminuser/sendmail', 'AdminUserController@sendmail')->name('adminuser.sendmail');
   Route::resource('user', 'UserController');
+  Route::POST('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+  Route::GET('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+  Route::POST('password/reset','Auth\ResetPasswordController@reset')->name('password.request');
+  Route::GET('password/reset/{token}','Auth\ResetPasswordController@showResetForm')->name('password.reset');
 
   Route::resource('user', UserController::class);
   Route::resource('schedule', WorkScheduleController::class);
