ae5483736bd7dd20e222e00d30d848a989b0ee2a
ryoryou <atsut001@cougars.csusm.edu>
2017-04-23 18:56:06 -0700

    Update: Data of the Daily Report have been set properly

--- database/seeds/DailyReportsTableSeeder.php
+++ database/seeds/DailyReportsTableSeeder.php
@@ -15,7 +15,40 @@ class DailyReportsTableSeeder extends Seeder
                 'user_id' => 1,
                 'title' => '初日',
                 'contents' => '研修初日でした。',
+                'reporting_time' => Carbon::create('2016','1','20'),
+            ]
+        );
+        DailyReports::create(
+            [
+                'user_id' => 1,
+                'title' => 'テスト１',
+                'contents' => 'これはテスト１です。',
+                'reporting_time' => Carbon::create('2016','3','20'),
+            ]
+        );
+        DailyReports::create(
+            [
+                'user_id' => 1,
+                'title' => 'テスト２',
+                'contents' => 'これはテスト２です。',
+                'reporting_time' => Carbon::create('2016','3','25'),
+            ]
+        );
+        DailyReports::create(
+            [
+                'user_id' => 1,
+                'title' => 'テスト３',
+                'contents' => 'これはテスト３です。',
                 'reporting_time' => Carbon::create('2017','3','20'),
-            ]);
+            ]
+        );
+        DailyReports::create(
+            [
+                'user_id' => 1,
+                'title' => 'テスト４',
+                'contents' => 'これはテスト４です。',
+                'reporting_time' => Carbon::create('2017','4','16'),
+            ]
+        );
     }
 }
