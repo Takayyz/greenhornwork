8eeced4da07c8c0ab6e115a8d5b3b2e84418e833
Atsushi <atsushi88885@gmail.com>
2017-04-24 11:36:26 +0900

    ルート修正

--- routes/web.php
+++ routes/web.php
@@ -25,22 +25,22 @@ Route::group(['prefix' => '/'], function() {
 });
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.' ,'namespace' => 'Admin'], function() {
-  Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-  Route::post('login', 'Auth\LoginController@login')->name('login');
-  Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+  Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+  Route::post('login', ['as' => 'login', 'uses' => 'Auth\LoginController@login']);
+  Route::post('logout', 'Auth\LoginController@logout');
 
   Route::get('/', 'HomeController@index');
   Route::resource('report', DailyReportController::class);
   Route::resource('store', StoreController::class);
 
   Route::resource('adminuser', AdminUserController::class);
-  Route::get('adminuser/{adminuser}/mailedit', 'AdminUserController@mailedit')->name('adminuser.mailedit');
-  Route::post('adminuser/sendmail', 'AdminUserController@sendmail')->name('adminuser.sendmail');
+  Route::get('adminuser/{adminuser}/mailedit', ['as' => 'adminuser.mailedit', 'AdminUserController@mailedit']);
+  Route::post('adminuser/sendmail', ['as' => 'adminuser.sendmail', 'uses' => 'AdminUserController@sendmail']);
   Route::resource('user', 'UserController');
-  Route::POST('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-  Route::GET('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-  Route::POST('password/reset','Auth\ResetPasswordController@reset')->name('password.request');
-  Route::GET('password/reset/{token}','Auth\ResetPasswordController@showResetForm')->name('password.reset');
+  Route::POST('password/email',['as' => 'password.email', 'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail']);
+  Route::GET('password/reset',['as' => 'password.request', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
+  Route::POST('password/reset', ['as' => 'password.request', 'uses' => 'Auth\ResetPasswordController@reset']);
+  Route::GET('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
 
   Route::resource('user', UserController::class);
   Route::resource('schedule', WorkScheduleController::class);
