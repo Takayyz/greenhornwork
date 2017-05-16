3cbffbcd41ae643c84853032739e2440ce9e2da9
ryoryou <atsut001@cougars.csusm.edu>
2017-05-15 20:24:45 -0700

    Code Cleanup and Remove: fix indent and remove Cryptogram

--- app/Mail/ApplicationMail.php
+++ app/Mail/ApplicationMail.php
@@ -7,7 +7,6 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Repositories\UserInfosRepository;
-use App\Services\Classes\Cryptogram;
 
 class ApplicationMail extends Mailable
 {
@@ -30,24 +29,24 @@ class ApplicationMail extends Mailable
      */
     public function build()
     {
-        $user_name = $this->inputs['last_name'] . ' ' . $this->inputs['first_name'];
-        $message = $this->inputs['message'];
+      $user_name = $this->inputs['last_name'] . ' ' . $this->inputs['first_name'];
+      $message = $this->inputs['message'];
 
-        // ネットワーク上で個人情報を第三者に盗み見られないように暗号化
-        $encoded_data = $this->easyEncryption($this->inputs);
+      // ネットワーク上で個人情報を第三者に盗み見られないように暗号化
+      $encoded_data = $this->easyEncryption($this->inputs);
 
-        // 送信するデータを設定（message, user_id, user_name）
-        $query_string = '?data=' . $encoded_data;
+      // 送信するデータを設定（message, user_id, user_name）
+      $query_string = '?data=' . $encoded_data;
 
-        // URLに設定したデータを付与
-        $url = url('/');
-        $data = [
-            'url' => $url,
-            'query_string' => $query_string,
-            'user_name' => $user_name,
-            'user_message' => $message
-        ];
-        return $this->view('mail.access_permission_email')->with($data);
+      // URLに設定したデータを付与
+      $url = url('/');
+      $data = [
+          'url' => $url,
+          'query_string' => $query_string,
+          'user_name' => $user_name,
+          'user_message' => $message
+      ];
+      return $this->view('mail.access_permission_email')->with($data);
     }
 
     /**
