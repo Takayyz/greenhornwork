c8e9c1bd1ccb8da262807770f5fb59685bc391fd
DESKTOP-G4TO403\l24ar <a24arc.ad@gmail.com>
2017-07-31 18:09:41 +0900

    Slack認証機能。以下ファイル内の不必要なコメントアウトを消去しました。SlackProvider.php / app.php 2017-07-31

--- app/Socialite/Two/SlackProvider.php
+++ app/Socialite/Two/SlackProvider.php
@@ -14,8 +14,6 @@ class SlackProvider extends AbstractProvider implements ProviderInterface
         if (count($this->scopes) > 0) {
            return $this->scopes;
         }
-        // Provide some default scopes if the user didn't define some.
-        // See: https://github.com/SocialiteProviders/Providers/pull/53
         return ['identity.basic', 'identity.email', 'identity.team'];
     }
 
@@ -26,30 +24,16 @@ class SlackProvider extends AbstractProvider implements ProviderInterface
 
     protected function getTokenUrl()
     {
-        // return 'https://api.line.me/v1/oauth/accessToken';
         return 'https://slack.com/api/oauth.access';
     }
 
     protected function getUserByToken($token)
     {
-        // $response = $this->getHttpClient()->get('https://api.line.me/v1/profile', [
-        //     'headers' => [
-        //         'X-Line-ChannelToken' => $token,
-        //     ],
-        // ]);
         $response = $this->getHttpClient()->get(
                 'https://slack.com/api/users.identity?token='.$token
             );
         return json_decode($response->getBody(), true);
     }
-    // protected function mapUserToObject(array $user)
-    // {
-    //     return (new User())->setRaw($user)->map([
-    //         'id'     => $user['mid'],
-    //         'name'   => $user['displayName'],
-    //         'avatar' => $user['pictureUrl'],
-    //     ]);
-    // }
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
@@ -60,14 +44,4 @@ class SlackProvider extends AbstractProvider implements ProviderInterface
             'organization_id' => array_get($user, 'team.id'),
         ]);
     }
-    // protected function getTokenFields($code)
-    // {
-    //     return [
-    //         'client_id'     => $this->clientId,
-    //         'client_secret' => $this->clientSecret,
-    //         'code'          => $code,
-    //         'redirect_uri'  => $this->redirectUrl,
-    //         'grant_type'    => 'authorization_code',
-    //     ];
-    // }
 }
